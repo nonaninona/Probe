c0ee835c7ac9b37428cf267390495432a7b01432
nonaninona <starcraft0529@gmail.com>
2024-09-29 01:54:52 +0900

    test : ArticleItemTest 통과

--- src/__tests__/components/ArticleItemTest.tsx
+++ src/__tests__/components/ArticleItemTest.tsx
@@ -1,15 +1,15 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import ArticleItemTest from '../../components/ChatbotPrompt';
+import ArticleItem from '../../components/ArticleItem';
 
 describe('ArticleItem render', () => {
     it('제목이 잘 그려지는가?', () => {
-        render(<ArticleItem/>);
+        render(<ArticleItem width={500} height={500} title={'title'} body={'body'}/>);
         const titleText = screen.getByText('title');
         expect(titleText).toBeInTheDocument();
     })
     it('내용이 잘 그려지는가?', () => {
-        render(<ArticleItem/>);
+        render(<ArticleItem width={500} height={500} title={'title'} body={'body'}/>);
         const bodyText = screen.getByText('body');
         expect(bodyText).toBeInTheDocument();
     })
--- src/components/ArticleItem.tsx
+++ src/components/ArticleItem.tsx
@@ -7,21 +7,15 @@ interface ArticleItemProp {
     body: string
 }
 
-export default function ArticleItem(prop : ArticleItemProp) {
+export default function ArticleItem(props : ArticleItemProp) {
     return (
-        <div style={ {width : prop.width, height : prop.height}} className={styles['article-item']}>
+        <div style={ {width : props.width, height : props.height}} className={styles['article-item']}>
             <div className={styles['content-wrapper']}>
                 <div className={styles['title']}>
-                    왕초보를 위한 어쩌고
+                    {props.title}
                 </div>
                 <div className={styles['body']}>
-                    정비사업이라는 말을 들어본 적 있는가?
-
-                    아마 익숙치 않은 사람들이 많을 것이다.
-
-                    하지만 재개발, 이라는 단어는 모두에게 익숙할 것이다.
-
-                    정비사업은 노후된 도시 지역을 새롭게 정비하여 주거 환경...
+                    {props.body}
                 </div>
             </div>
         </div>
