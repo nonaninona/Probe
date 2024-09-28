eebcde50740d5ae29554a33b1f6b03b1baeb365e
nonaninona <starcraft0529@gmail.com>
2024-09-29 00:36:48 +0900

    feat : ChatPage에 handleQuery 추가

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -1,9 +1,12 @@
+import { useNavigate } from "react-router-dom"
 import ChatbotPrompt from "../components/ChatbotPrompt"
 import LastChatList from "../components/ChatRoom/LastChatList"
 import NavBar from "../components/NavBar"
 import styles from "./ChatPage.module.scss"
 
 export function ChatPage() {
+    const navigate = useNavigate();
+
     const dummyItems = [
         {
             chatRoomId : 1,
@@ -34,12 +37,18 @@ export function ChatPage() {
 
     }
 
+    const handleQuery = (query: string) => {
+        navigate("/chatroom")
+        console.log(query);
+        //새로운 채팅방 + 그 화면으로 이동 + 쿼리 던지고 받기
+    }
+
     return (
         <div className={styles['chat-page']}>
             <NavBar />
             <div className={styles['first-ui']}>
                 <div className={styles['notice']}>프로비에게 질문하기</div>
-                <ChatbotPrompt />
+                <ChatbotPrompt onQuery={handleQuery} />
             </div>
             <LastChatList items={dummyItems}/>
         </div>
