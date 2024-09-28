5fd50833733f6f025874fccf9fa46d422187d2f9
nonaninona <53222283+nonaninona@users.noreply.github.com>
2024-09-28 23:23:01 +0900

    feat : Add LastChatBox component (#13)
    * test : LastChatBoxTest 작성
    
    * feat : LastChatBox 구현
    
    * test : LastChatBoxTest 통과

---
+++ src/__tests__/components/LastChatBoxTest.tsx
@@ -0,0 +1,11 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import LastChatBox from '../../components/LastChatBox';
+
+describe('ChatbotPrompt render', () => {
+    it('제목이 잘 그려지는가?', () => {
+        render(<LastChatBox chatRoomId={1} title={"테스트 제목"} date={'3일전'}/>);
+        const title = screen.getByText('테스트 제목');
+        expect(title).toBeInTheDocument();
+    })
+})
---
+++ src/components/LastChatBox.tsx
@@ -0,0 +1,26 @@
+import styles from '../components/LastChatBox.module.scss'
+import probeE from '../assets/ProbeE.png'
+
+interface LastChatBoxProps {
+    chatRoomId: number,
+    title: string,
+    date: string,
+    onClick?: Function
+}
+
+export default function LastChatBox({ chatRoomId, title, date, onClick }: LastChatBoxProps) {
+
+    const handleClick = () => {
+        onClick!(chatRoomId)
+    }
+
+    return (
+        <div onClick={handleClick} className={styles['last-chat-box']}>
+            <div className={styles['content-container']}>
+                <img className={styles['probe-E-image']} src={probeE}/>
+                <div className={styles['title']}>{title}</div>
+                <div className={styles['last-update']}>{date}</div>
+            </div>
+        </div>
+    )
+}
