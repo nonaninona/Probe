d2211d72e80fda2611a87efa93354cdee51b8bbc
nonaninona <starcraft0529@gmail.com>
2024-09-28 16:59:51 +0900

    feat : id, password state 추가 및 onLogin으로 전달, onLogin 타입 변경

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,28 +1,37 @@
 import styles from './LoginForm.module.scss';
+import { useState } from 'react';
 
 interface LoginFromProps {
-    onLogin? : React.MouseEventHandler<HTMLButtonElement>,
+    onLogin? : Function,
     errorMessage: string
 }
 
 export default function LoginForm(props : LoginFromProps) {
+    const [id, setId] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        props.onLogin!!({id, password})
+    }
+
     return (
         <div className={styles['login-form']}>
             <div className={styles['title']} data-testid='로그인-제목'>로그인</div>
             <div className={styles['item']}>
                 <div className={styles['item-title']}>아이디</div>
                 <div className={styles['item-input']}>
-                    <input className={styles['item-input-field']} placeholder='아이디를 입력해주세요' type="text"></input>
+                    <input className={styles['item-input-field']} placeholder='아이디를 입력해주세요' type="text" onChange={(e)=>setId(e.target.value)} value={id}></input>
                 </div>
             </div>
             <div className={styles['item']}>
                 <div className={styles['item-title']}>비밀번호</div>
                 <div className={styles['item-input']}>
-                    <input className={styles['item-input-field']} placeholder='비밀번호를 입력해주세요' type="password"></input>
+                    <input className={styles['item-input-field']} placeholder='비밀번호를 입력해주세요' type="password" onChange={(e)=>setPassword(e.target.value)} value={password}></input>
                 </div>
             </div>
             <div className={styles['error-message']}>{props.errorMessage}</div>
-            <button onClick={props.onLogin} className={styles['login-button']}>로그인</button>
+            <button onClick={handleLogin} className={styles['login-button']}>로그인</button>
         </div>
     )
 }
