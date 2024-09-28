f3ba885609f918de474b723a826be6d92c0c564f
nonaninona <53222283+nonaninona@users.noreply.github.com>
2024-09-28 20:10:07 +0900

    feat : Add ChatbotPrompt component (#8)
    * test : ChatPromptTest 작성 완료
    
    * feat : ChatPrompt Component 작성
    
    * test : ChatbotPrompt 테스트 통과
    
    img에 alt 텍스트 추가

---
+++ src/components/ChatbotPrompt.tsx
@@ -0,0 +1,23 @@
+import styles from '../components/ChatbotPrompt.module.scss'
+import promptArrow from '../assets/promptArrow.svg';
+import { useState } from 'react';
+
+interface ChatbotPromptProps {
+    onQuery? : Function
+}
+
+export default function ChatbotPrompt({onQuery} : ChatbotPromptProps) {
+    const [query, setQuery] = useState('');
+
+    const handleQuery = (e) => {
+        e.preventDefault();
+        onQuery!(query)
+    }
+
+    return (
+        <div className={styles['chat-prompt']}>
+            <img alt='전송버튼' onClick={handleQuery} src={promptArrow}></img>
+            <input className={styles['prompt-input-field']} placeholder='프로비에게 부동산 물어보기 ex) 정비사업 종류에는 뭐가 있어?' type="text" onChange={(e) => setQuery(e.target.value)} value={query}></input>
+        </div>
+    )
+}
