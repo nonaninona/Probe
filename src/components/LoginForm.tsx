e8dbb7868cf48a9189ab05cb71261ab02e06eacd
nonaninona <starcraft0529@gmail.com>
2024-09-28 16:34:08 +0900

    style : LoginForm input 태그 플레이스 홀더 추가

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -8,17 +8,17 @@ interface LoginFromProps {
 export default function LoginForm(props : LoginFromProps) {
     return (
         <div className={styles['login-form']}>
-            <div className={styles['title']}>로그인</div>
+            <div className={styles['title']} data-testid='로그인-제목'>로그인</div>
             <div className={styles['item']}>
                 <div className={styles['item-title']}>아이디</div>
                 <div className={styles['item-input']}>
-                    <input className={styles['item-input-field']} type="text"></input>
+                    <input className={styles['item-input-field']} placeholder='아이디를 입력해주세요' type="text"></input>
                 </div>
             </div>
             <div className={styles['item']}>
                 <div className={styles['item-title']}>비밀번호</div>
                 <div className={styles['item-input']}>
-                    <input className={styles['item-input-field']} type="password"></input>
+                    <input className={styles['item-input-field']} placeholder='비밀번호를 입력해주세요' type="password"></input>
                 </div>
             </div>
             <div className={styles['error-message']}>{props.errorMessage}</div>
