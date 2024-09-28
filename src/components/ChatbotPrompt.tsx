1fa9650fe5ef218ec165298bc6df523e73007766
nonaninona <starcraft0529@gmail.com>
2024-09-28 20:09:07 +0900

    test : ChatbotPrompt 테스트 통과
    img에 alt 텍스트 추가

--- src/components/ChatbotPrompt.tsx
+++ src/components/ChatbotPrompt.tsx
@@ -16,7 +16,7 @@ export default function ChatbotPrompt({onQuery} : ChatbotPromptProps) {
 
     return (
         <div className={styles['chat-prompt']}>
-            <img onClick={handleQuery}  src={promptArrow}></img>
+            <img alt='전송버튼' onClick={handleQuery} src={promptArrow}></img>
             <input className={styles['prompt-input-field']} placeholder='프로비에게 부동산 물어보기 ex) 정비사업 종류에는 뭐가 있어?' type="text" onChange={(e) => setQuery(e.target.value)} value={query}></input>
         </div>
     )
