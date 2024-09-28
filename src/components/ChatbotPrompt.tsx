285907b1818c54df256c2f3cf50f1f4b2ef56411
nonaninona <starcraft0529@gmail.com>
2024-09-28 21:50:18 +0900

    fix : img에는 disabled 속성이 적용되지 않아 ChatbotPrompt에서 img -> button 변경

--- src/components/ChatbotPrompt.tsx
+++ src/components/ChatbotPrompt.tsx
@@ -1,22 +1,38 @@
 import styles from '../components/ChatbotPrompt.module.scss'
-import promptArrow from '../assets/promptArrow.svg';
 import { useState } from 'react';
 
 interface ChatbotPromptProps {
-    onQuery? : Function
+    onQuery?: Function
 }
 
-export default function ChatbotPrompt({onQuery} : ChatbotPromptProps) {
+export default function ChatbotPrompt({ onQuery }: ChatbotPromptProps) {
     const [query, setQuery] = useState('');
-
+    var isDisable = query == '';
+    
     const handleQuery = () => {
         onQuery!(query)
     }
 
+    const handleChange = (value: string) => {
+        setQuery(value)
+        if (query == '')
+            isDisable = true
+        else
+            isDisable = false
+        console.log(value)
+        console.log(isDisable)
+    }
+
     return (
         <div className={styles['chat-prompt']}>
-            <img alt='전송버튼' onClick={handleQuery} src={promptArrow}></img>
-            <input className={styles['prompt-input-field']} placeholder='프로비에게 부동산 물어보기 ex) 정비사업 종류에는 뭐가 있어?' type="text" onChange={(e) => setQuery(e.target.value)} value={query}></input>
+            <button className={styles['query-button']} disabled={isDisable} onClick={handleQuery} />
+            <input
+                className={styles['prompt-input-field']}
+                placeholder='프로비에게 부동산 물어보기 ex) 정비사업 종류에는 뭐가 있어?'
+                type="text"
+                onChange={(e) => handleChange(e.target.value)}
+                value={query} 
+                />
         </div>
     )
 }
