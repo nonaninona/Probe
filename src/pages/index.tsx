9f556a31ccf995bd39c6f0105153436460412b02
nonaninona <starcraft0529@gmail.com>
2024-09-29 00:15:24 +0900

    file : chat 폴더 삭제, ChatPage 경로 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
 export { HomePage } from './HomePage';
-export { ChatPage } from './chat/ChatPage';
+export { ChatPage } from './ChatPage';
 export { ArticlePage } from './article/ArticlePage';
 export { MapPage } from './map/MapPage';
