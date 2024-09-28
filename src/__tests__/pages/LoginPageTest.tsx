37a63acba518ba6174774c7dd526321cc392314f
nonaninona <53222283+nonaninona@users.noreply.github.com>
2024-09-28 17:32:59 +0900

    feat : LoginPage 컴포넌트 추가 (#5)
    * test : LoginPageTest 작성 완료
    
    * feat : LoginPage 컴포넌트 작성
    
    * test : LoginPageTest 통과

---
+++ src/__tests__/pages/LoginPageTest.tsx
@@ -0,0 +1,47 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { MemoryRouter, Routes, Route } from 'react-router-dom';
+import { LoginPage } from '../../pages/LoginPage';
+
+describe('LoginPage 로그인 기능 테스트', () => {
+    it('올바르지 않은 아이디, 비밀번호 입력 후 요청 시, 오류 메시지가 뜨는가?', () => {
+        render(
+            <MemoryRouter initialEntries={['/login']}>
+                <Routes>
+                    <Route path="/login" element={ <LoginPage /> }/>
+                    <Route path="/" element={ <div>메인</div> }/>
+                </Routes>
+            </MemoryRouter>
+        );
+
+
+        const loginBtn = screen.getByRole("button")
+        fireEvent.click(loginBtn);
+
+        const errorMsg = screen.getByText("아이디 혹은 비밀번호가 올바르지 않습니다")
+
+        expect(errorMsg).toBeInTheDocument();
+    })
+    it('올바른 아이디, 비밀번호 입력 후 요청 시 HomePage로 이동하는가?', () => {
+        render(
+            <MemoryRouter initialEntries={['/login']}>
+                <Routes>
+                    <Route path="/login" element={ <LoginPage /> }/>
+                    <Route path="/" element={ <div>메인</div> }/>
+                </Routes>
+            </MemoryRouter>
+        );
+
+        const idInput = screen.getByPlaceholderText('아이디를 입력해주세요');
+        const passwordInput = screen.getByPlaceholderText('비밀번호를 입력해주세요');
+        fireEvent.change(idInput, { target: { value: 'test@example.com' } });
+        fireEvent.change(passwordInput, { target: { value: 'password123' } });
+
+        const loginBtn = screen.getByRole('button');
+        fireEvent.click(loginBtn);
+
+        const pageName = screen.getByText('메인')
+        expect(pageName).toBeInTheDocument();
+        
+    })
+})
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,38 @@
+import { useNavigate } from "react-router-dom";
+import LoginForm from "../components/LoginForm"
+import NavBar from "../components/NavBar"
+import { useState } from "react";
+import styles from './LoginPages.module.scss';
+
+export function LoginPage() {
+    const navigate = useNavigate();
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const callLoginAPI = (id: string, password: string) => {
+        console.log(id);
+        console.log(password);
+        
+        const response = { ok : false }
+        if(id == 'test@example.com' && password == 'password123')
+            response.ok = true;
+
+        return response
+    }
+
+    const handleLogin = ({id, password} : { id: string; password: string }) => {
+        const response = callLoginAPI(id, password);
+        if(response.ok) {
+            setErrorMessage('')
+            navigate('/')
+        }
+        setErrorMessage('아이디 혹은 비밀번호가 올바르지 않습니다')
+    }
+    return ( 
+        <div className={styles['login-page']}>
+            <NavBar />
+            <div className={'login-form-wrapper'}>
+                <LoginForm onLogin={handleLogin} errorMessage={errorMessage}/>
+            </div>
+        </div>
+    )
+}
