551a26debce65fbb91cbec28a6b9bf6cfcb0245d
nonaninona <starcraft0529@gmail.com>
2024-09-28 21:03:21 +0900

    file : HomePage 폴더 생성, HomePageUI 컴포넌트 해당 폴더로 이동

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,19 @@
+import Footer from "../components/Footer";
+import MainPageUI0 from "../components/HomePage/HomePageUI0";
+import MainPageUI1 from "../components/HomePage/HomePageUI1";
+import MainPageUI2 from "../components/HomePage/HomePageUI2";
+import NavBar from "../components/NavBar";
+import styles from "./HomePage.module.scss"
+
 export function HomePage() {
     return (
-        <></>
+        <div className={styles['home-page']}>
+            <NavBar/>
+            <MainPageUI0/>
+            <MainPageUI1/>
+            <MainPageUI2/>
+            <Footer/>
+        </div>
+
     )
 }
