d3a069e6a664796c9dd4fc3d62c0180ee1d88dcb
nonaninona <starcraft0529@gmail.com>
2024-09-29 13:37:07 +0900

    feat : ArticlePage 작성

---
+++ src/pages/ArticlePage.tsx
@@ -0,0 +1,41 @@
+import NavBar from '../components/NavBar';
+import RecommendArticleList from '../components/RecommendArticleList';
+import styles from './ArticlePage.module.scss';
+
+export function ArticlePage() {
+
+    const callGetAPI = () => {
+
+    }
+
+    const dummyItems = [
+        {
+            articleId : 1,
+            width : 580,
+            height : 478,
+            title : 'test title1',
+            body : 'test body1'
+        },
+        {
+            articleId : 2,
+            width : 580,
+            height : 228,
+            title : 'test title2',
+            body : 'test body2'
+        },
+        {
+            articleId : 3,
+            width : 580,
+            height : 228    ,
+            title : 'test title3',
+            body : 'test body3'
+        }
+    ]
+
+    return (
+        <div className={styles['article-page']}>
+            <NavBar />
+            <RecommendArticleList items={dummyItems}/>
+        </div>
+    )
+}
