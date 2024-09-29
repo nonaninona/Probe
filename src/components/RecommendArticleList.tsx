fd3710f011b9e4df5590cffa6531f76a5fa53790
nonaninona <starcraft0529@gmail.com>
2024-09-29 11:59:03 +0900

    feat : ArticleItem에 onClick을 넘겨줌

--- src/components/RecommendArticleList.tsx
+++ src/components/RecommendArticleList.tsx
@@ -1,11 +1,24 @@
 import styles from './Recommend.module.scss';
 import ArticleItem, { ArticleItemProps } from './ArticleItem';
+import { useNavigate } from 'react-router-dom';
 
 interface RecommendArticleListProps {
     items: ArticleItemProps[]
 }
 
 export default function RecommendArticleList(props: RecommendArticleListProps) {
+    const navigate = useNavigate();
+
+
+    const handleArticleItemClick = () => {
+        navigate('/articledetail')
+    }
+
+    const handleMoreArticleClick = () => {
+        navigate('/articlelist')
+    }
+
+
     return (
         <div className={styles['recommend-article-list']}>
             <div className={styles['content-wrapper']}>
@@ -13,13 +26,13 @@ export default function RecommendArticleList(props: RecommendArticleListProps) {
                 <div className={styles['article-wrapper']}>
                     {
                         props.items.map((item) => {
-                            return <div className={styles['article-item']}>
-                                <ArticleItem width={item.width} height={item.height} title={item.title} body={item.body} />
+                            return <div key={item.articleId} className={styles['article-item']}>
+                                <ArticleItem articleId={item.articleId} onClick={handleArticleItemClick} width={item.width} height={item.height} title={item.title} body={item.body} />
                             </div>
                         })
                     }
                 </div>
-                <div className={styles['more-article-button']}>추천 아티클 더 보기 {'>'}</div>
+                <div onClick={handleMoreArticleClick} className={styles['more-article-button']}>추천 아티클 더 보기 {'>'}</div>
             </div>
         </div>
     )
