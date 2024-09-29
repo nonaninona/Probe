6936c3f8d0da9c4a5f7e4a2204e0ab67f384cab2
nonaninona <53222283+nonaninona@users.noreply.github.com>
2024-09-29 14:17:22 +0900

    feat : add SignUpForm component (#20)
    * test : SignUpFormTest 작성
    
    * feat : SignUpForm 컴포넌트 작성
    
    * test : SignUpFormTest 통과

---
+++ src/components/SignUpForm.tsx
@@ -0,0 +1,43 @@
+import styles from './SignUpForm.module.scss';
+import { useState } from 'react';
+
+interface SignUpFormProps {
+    onLogin? : Function,
+    errorMessage: string
+}
+
+export default function SignUpForm(props : SignUpFormProps) {
+    const [userName, setUserName] = useState('');
+    const [id, setId] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSignUp = () => {
+        props.onLogin!!({userName, id, password})
+    }
+
+    return (
+        <div className={styles['login-form']}>
+            <div className={styles['title']} data-testid='회원가입-제목'>회원가입</div>
+            <div className={styles['item']}>
+                <div className={styles['item-title']}>이름</div>
+                <div className={styles['item-input']}>
+                    <input className={styles['item-input-field']} placeholder='이름을 입력해주세요' type="text" onChange={(e)=>setUserName(e.target.value)} value={userName}></input>
+                </div>
+            </div>
+            <div className={styles['item']}>
+                <div className={styles['item-title']}>아이디</div>
+                <div className={styles['item-input']}>
+                    <input className={styles['item-input-field']} placeholder='아이디를 입력해주세요' type="text" onChange={(e)=>setId(e.target.value)} value={id}></input>
+                </div>
+            </div>
+            <div className={styles['item']}>
+                <div className={styles['item-title']}>비밀번호</div>
+                <div className={styles['item-input']}>
+                    <input className={styles['item-input-field']} placeholder='비밀번호를 입력해주세요' type="password" onChange={(e)=>setPassword(e.target.value)} value={password}></input>
+                </div>
+            </div>
+            <div className={styles['error-message']}>{props.errorMessage}</div>
+            <button onClick={handleSignUp} className={styles['login-button']}>회원가입</button>
+        </div>
+    )
+}
