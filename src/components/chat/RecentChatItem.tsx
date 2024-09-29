21d9b2c2fc4ba098c495df82dd2bc6fd3cd8f669
nonaninona <starcraft0529@gmail.com>
2024-09-29 17:26:28 +0900

    feat : RecentChatItem 작성 완료

---
+++ src/components/chat/RecentChatItem.tsx
@@ -0,0 +1,24 @@
+import styles from './RecentChatItem.module.scss'
+import chatIcon from '../../assets/chat-icon.svg';
+
+interface RecentChatItemProps {
+    chatRoomId: number,
+    title: string,
+    onClick?: Function
+}
+
+export default function RecentChatItem({ chatRoomId, title, onClick }: RecentChatItemProps) {
+
+    const handleClick = () => {
+        onClick!(chatRoomId)
+    }
+
+    return (
+        <div onClick={handleClick} className={styles['recent-chat-item']}>
+            <img className={styles['chat-image']} src={chatIcon}></img>
+            <div className={styles['chat-title']}>
+                {title}
+            </div>
+        </div>
+    )
+}
