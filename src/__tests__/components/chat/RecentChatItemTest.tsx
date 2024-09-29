ebf46fe9d32939bc848c3a414b75b1ddc564215a
nonaninona <starcraft0529@gmail.com>
2024-09-29 17:27:24 +0900

    test : RecentChatItemTest 통과

--- src/__tests__/components/chat/RecentChatItemTest.tsx
+++ src/__tests__/components/chat/RecentChatItemTest.tsx
@@ -1,10 +1,11 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import RecentChatItem from '../../../components/chat/RecentChatItem';
 
 describe('RecentChatItem render', () => {
     it('제목이 잘 그려지는가?', () => {
-        render(<RecentChatItem title={}/>);
-        const title = screen.getByText('test title1');
+        render(<RecentChatItem title='test title' chatRoomId={1}/>);
+        const title = screen.getByText('test title');
         expect(title).toBeInTheDocument();
     })
 })
