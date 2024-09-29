95908e81d40fa01dcff679ed7717b23a2f7b8342
nonaninona <starcraft0529@gmail.com>
2024-09-29 17:03:51 +0900

    file : login 디렉토리 추가 및 파일 위치 이동

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import LoginForm from "../components/LoginForm"
+import LoginForm from "../components/login/LoginForm"
 import NavBar from "../components/NavBar"
 import { useState } from "react";
 import styles from './LoginPage.module.scss';
