19d54e19b54349cb8382842003349eb60ba09f31
nonaninona <starcraft0529@gmail.com>
2024-09-30 15:51:00 +0900

    fix : vercel 배포를 위한 빌드 오류 수정

--- src/__tests__/pages/AppTest.tsx
+++ src/__tests__/pages/AppTest.tsx
@@ -1,7 +1,7 @@
 // 클릭 시 라우터를 통해 이동한 걸 테스트 할 수 있나? screen.getText 이런 걸로.... 하면 그런데 현재 있는 더미 텍스트를 변경하면 터지겠군
 // 테스트 케이스 1 -> 최초에 App.tsx가 렌더링 되면, HomePage가 잘 뜨는가?
 
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { BrowserRouter } from 'react-router-dom'
 import App from '../../App';
