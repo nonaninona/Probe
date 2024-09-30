e528aa6fc434a76b93237112df08b372ef686ddb
nonaninona <starcraft0529@gmail.com>
2024-09-30 15:40:48 +0900

    style : ChatPage 스타일 수정

--- src/components/chat/LastChatList.tsx
+++ src/components/chat/LastChatList.tsx
@@ -17,18 +17,20 @@ export default function LastChatList({ items }: LastChatListProps) {
 
     return (
         <div className={styles['last-chat-list']}>
-            <div className={styles['title']}>이전 대화 목록</div>
-            <div className={styles['list-wrapper']}>
-                {items.map((item) => {
-                    return <div key={item.chatRoomId} className={styles['list-item']}>
-                        <LastChatBox
-                            chatRoomId={item.chatRoomId}
-                            title={item.title}
-                            date={item.date}
-                            onClick={handleClick}
-                        />
-                    </div>
-                })}
+            <div className={styles['content-wrapper']}>
+                <div className={styles['title']}>이전 대화 목록</div>
+                <div className={styles['list-wrapper']}>
+                    {items.map((item) => {
+                        return <div key={item.chatRoomId} className={styles['list-item']}>
+                            <LastChatBox
+                                chatRoomId={item.chatRoomId}
+                                title={item.title}
+                                date={item.date}
+                                onClick={handleClick}
+                            />
+                        </div>
+                    })}
+                </div>
             </div>
         </div>
     )
