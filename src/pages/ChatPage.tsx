8459ff562297a2686f08ca1fdc2e8bb39a33b04d
nonaninona <starcraft0529@gmail.com>
2024-09-30 15:16:15 +0900

    feat : chat page에서 chat room 리스트 불러오는 api 호출

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -3,39 +3,55 @@ import ChatbotPrompt from "../components/ChatbotPrompt"
 import LastChatList from "../components/chat/LastChatList"
 import NavBar from "../components/NavBar"
 import styles from "./ChatPage.module.scss"
+import { useEffect } from "react"
+import { callGetChatRoomListAPI } from "../services/ChatAPI"
 
 export function ChatPage() {
+
     const navigate = useNavigate();
+
+    const id = localStorage.getItem('id')
 
     const dummyItems = [
         {
-            chatRoomId : 1,
-            title : "test title 1",
-            date : "1 days ago"
+            chatRoomId: 1,
+            title: "test title 1",
+            date: "1 days ago"
         },
-    
+
         {
-            chatRoomId : 2,
-            title : "test title 2",
-            date : "2 days ago"
+            chatRoomId: 2,
+            title: "test title 2",
+            date: "2 days ago"
         },
-    
+
         {
-            chatRoomId : 3,
-            title : "test title 3",
-            date : "3 days ago"
+            chatRoomId: 3,
+            title: "test title 3",
+            date: "3 days ago"
         },
-    
+
         {
-            chatRoomId : 4,
-            title : "test title 4",
-            date : "4 days ago"
+            chatRoomId: 4,
+            title: "test title 4",
+            date: "4 days ago"
         }
     ]
 
-    const callGetItems = () => {
+    useEffect(() => {
+        console.log(id)
 
-    }
+        if (id == null)
+            return
+
+        callGetChatRoomListAPI({ id: id! })
+            .then((data) => {
+                console.log(data)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }, [])
 
     const handleQuery = (query: string) => {
         navigate("/newchat")
@@ -50,7 +66,7 @@ export function ChatPage() {
                 <div className={styles['notice']}>프로비에게 질문하기</div>
                 <ChatbotPrompt onQuery={handleQuery} />
             </div>
-            <LastChatList items={dummyItems}/>
+            <LastChatList items={dummyItems} />
         </div>
     )
 }
