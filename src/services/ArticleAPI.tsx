34f2749ef07d8b436524e06560a8c751af4d63b9
nonaninona <starcraft0529@gmail.com>
2024-10-27 16:12:47 +0900

    feat : AritlceAPI 추가

---
+++ src/services/ArticleAPI.tsx
@@ -0,0 +1,69 @@
+export async function callGetRecommendedArticles({userName} : {userName:string}) {
+    const request = {
+        username : userName
+    }
+
+    const JWT = localStorage.getItem("JWT");
+    const response = await fetch(import.meta.env.VITE_APP_SERVER_URL + '/article/getPreferredArticles', { 
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json",
+            "Authorization" : JWT!
+        },
+        body: JSON.stringify(request)
+    })
+
+    if(!response.ok) {
+        const errMsg = await response.json();
+        return new Error(errMsg || 'failed to fetch data')
+    }
+
+    return response.json();
+}
+
+export async function callUpdateUserPreference({userName, articleId} : {userName:string, articleId:string}) {
+    const request = {
+        username : userName,
+        articleId : articleId
+    }
+
+    const JWT = localStorage.getItem("JWT");
+    const response = await fetch(import.meta.env.VITE_APP_SERVER_URL + '/member/adjustUserPreferenceRequest', { 
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json",
+            "Authorization" : JWT!
+        },
+        body : JSON.stringify(request)
+    })
+
+    if(!response.ok) {
+        const errMsg = await response.json();
+        return new Error(errMsg || 'failed to fetch data')
+    }
+
+    return response.json();
+}
+
+export async function callGetSimilarArticles({articleId} : {articleId:string}) {
+    const request = {
+        articleId : articleId
+    }
+
+    const JWT = localStorage.getItem("JWT");
+    const response = await fetch(import.meta.env.VITE_APP_SERVER_URL + '/article/getSimilarArticles', { 
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json",
+            "Authorization" : JWT!
+        },
+        body : JSON.stringify(request)
+    })
+
+    if(!response.ok) {
+        const errMsg = await response.json();
+        return new Error(errMsg || 'failed to fetch data')
+    }
+
+    return response.json();
+}
